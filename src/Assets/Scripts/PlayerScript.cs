98e03f2b54ca6a8dfa594760131badf9b67ed13d
imagire <imagire@gmail.com>
2024-09-11 10:50:25 +0900

    敵を話すことができた

--- src/Assets/Scripts/PlayerScript.cs
+++ src/Assets/Scripts/PlayerScript.cs
@@ -20,34 +20,37 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // �ێ�����
-        if (Input.GetKeyDown(KeyCode.C))
+        // 捕まえる
+        if (Input.GetKey(KeyCode.C))
         {
-            // �ێ����Ă��Ȃ���Γ�����
             if(catchingGO == null)
             {
+                // 捕まえていなければ捕まえる
                 if (catchableGO != null)
                 {
-                    catchingGO = catchableGO;// �߂܂���
+                    catchingGO = catchableGO;
                     catchingGO.transform.SetParent(this.gameObject.transform);
                 }
             }
-            else
-            {
-                catchingGO.transform.localPosition = new Vector3(0f, 1f, 1f);
-            }
         }
-        else// �ێ����Ȃ�
+        else if(catchingGO != null)
+        {
+            // 押していなくて敵を持っていれば投げる
+            catchingGO.transform.SetParent(null);
+            catchingGO = null;
+        }
+
+        // 捕まえている時はHPが下がる
+        if (catchingGO != null)
         {
             catchingGO.transform.localPosition = new Vector3(0f, 1f, 1f);
-                hp -= 1;
+            hp -= 1;
 
-                if (HPSlider != null)
-                {
-                    HPSlider.SetValue(hp);
-                    // UnityEngine.Debug.Log("�����Ă��鎞�̃_���[�W");
-                }
-        }
+            if (HPSlider != null)
+            {
+                HPSlider.SetValue(hp);
+            }
+       }
     }
 
     void OnControllerColliderHit(ControllerColliderHit col)
