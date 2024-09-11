9644aea96335fc220fc75d0bb7e0e133572e7e6b
shui-mitsuhashi <108126106+shui-mitsuhashi@users.noreply.github.com>
2024-09-11 10:42:29 +0900

    敵をプレハブ化しました

--- src/Assets/Scripts/Sample3.cs
+++ src/Assets/Scripts/Sample3.cs
@@ -6,25 +6,34 @@ public class Sample3 : MonoBehaviour
 {
     //GameObjectŒ^‚ğ•Ï”target‚ÅéŒ¾‚µ‚Ü‚·B
     public GameObject target;
+    float jumpForce = 4f;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Quaternion lookRotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up);
+        Vector3 direction = target.transform.position - transform.position;
+        direction.y = 0;  // Y²‚Ì‰ñ“]‚ğ–³‹‚µ‚Ä…•½–Ê‚Å‚Ì‰ñ“]‚É‚·‚é
 
-        lookRotation.z = 0;
-        lookRotation.x = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime);
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 1f);
 
-        Vector3 p = new Vector3(0f, 0f, 0.001f);
-
-        transform.Translate(p);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        
+        if (col.gameObject.tag == "Ground")
+        {
+            rb.velocity = Vector3.up * jumpForce;
+            Vector3 p = new Vector3(0f, 0f, 0.001f);
+            transform.Translate(p);
+        }
     }
 }
